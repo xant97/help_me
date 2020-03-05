11f476b0c43bbe46afede73c23fc3cb1655b67d6
xant97 <54405493+xant97@users.noreply.github.com>
2020-03-05 15:05:20 +0100

    now something works
    yep

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
+function myFunction() {
+  var node = document.createElement("option");
+  var textnode = document.createTextNode(getCoins());
+  node.appendChild(textnode);
+  document.getElementById("select").appendChild(node);
+} 
 function getCoins(){
-var url = new Request('https://api.coinpaprika.com/v1/coins');
+
+var url = new Request('https://api.coinpaprika.com/v1/coins/btc-bitcoin');
 return fetch(url)
-
-.then(response => {
-	    console.log(response.json());
+.then(response => response.json()
+/*
+.then(function(data) {
+let nomnom = data;
+return nomnom.map(function(ree){
+	let option = myFunction();
+	option.innerHTML = `{ree.id}`;
+	append(option, select);
 })
-
+})
+*/
 .catch(function(error) {
     console.log('nie dziala =)', error)
-});
-
+})
+)
 }
+
 console.log(getCoins());
