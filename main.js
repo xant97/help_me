a86dfeb26aabe529acd66749390a68c25125329f
xant97 <fixexirstut@gmail.com>
2020-03-05 19:20:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 function getCoins(){
-var url = new Request('https://api.coinpaprika.com/v1/coins');
+var url = new Request('https://api.coinpaprika.com/v1/coins/');
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+  const ul = document.getElementById('select');
 return fetch(url)
 
 .then(response => {
 	    console.log(response.json());
+})
+.then(function(data) {
+    let select = data.coins();
+    return select.map(function(author) {
+      let li = createNode('li'),
+          img = createNode('img'),
+          span = createNode('span');
+      img.src = author.picture.medium;
+      span.innerHTML = `${author.name.first} ${author.name.last}`;
+      append(li, img);
+      append(li, span);
+      append(ul, li);
+    }) 
 })
 
 .catch(function(error) {
