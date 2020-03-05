45fd62191dded87febfc6e879728f04c7355eff9
xant97 <fixexirstut@gmail.com>
2020-03-05 22:14:20 +0100

    eh
    dziala ale nie tak jak planowalem

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-  
-function myFunction() {
+ /*
+ function myFunction() {
   var node = document.createElement("option");
   var textnode = document.createTextNode(getCoins());
   node.appendChild(textnode);
@@ -9,8 +9,8 @@ function getCoins(){
 
 var url = new Request('https://api.coinpaprika.com/v1/coins/btc-bitcoin');
 return fetch(url)
-.then(response => response.json()
-/*
+.then(response => response.json())
+
 .then(function(data) {
 let nomnom = data;
 return nomnom.map(function(ree){
@@ -19,11 +19,33 @@ return nomnom.map(function(ree){
 	append(option, select);
 })
 })
-*/
+
 .catch(function(error) {
     console.log('nie dziala =)', error)
 })
-)
+
 }
 
 console.log(getCoins());
+*/
+function myFunction() {
+  var node = document.createElement("option");
+  var textnode = document.createTextNode();
+  node.appendChild(textnode);
+  document.getElementById("select").appendChild(node);
+} 
+
+//przykladowa obsluga, mozesz tu zrobic appenda
+var url = 'https://api.coinpaprika.com/v1/coins/';
+let coinList = fetch(url).then((response) => response.json())
+.then(function(data) {
+  //tu xD
+  
+  for (i = 0; i < 15/*data.length*/; i++) { 
+    console.log(data[i].id); 
+} 
+  //console.log(data[0].id);
+})
+.catch(function(error) {
+  console.log(error);
+}); 
