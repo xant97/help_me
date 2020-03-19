62279d808ad5f81883bdbd4430ed6db099893545
xant97 <fixexirstut@gmail.com>
2020-03-19 22:18:02 +0100

    wow

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let counter = 0;
 function createNode(element){
 	xd =  document.createElement(element); 
-	xd.id = (counter++)/2; // nie mma pojecia dlaczego /2 XD //edit: w sumie to sie domyslam
+	xd.id = counter; // smiiiga
 	return xd;
 							}
 function append(parent, el){
@@ -9,27 +9,40 @@ function append(parent, el){
 							}
 const ul = document.getElementById('select');
 const button = document.getElementById('button');
+const divik = document.getElementById('testowy');
 var url = 'https://api.coinpaprika.com/v1/coins/';
 let coinList = fetch(url).then((response) => response.json())
 .then(function siema(data) {
 let i = 0;
-  for(i; i<15 /*data.length*/; i++){ // 15 dla 15 | data.length dla wszystkich 
+  for(i; i<data.length; i++){ // 15 dla 15 | data.length dla wszystkich 
+  counter = counter+1;
 	let niewiem = data[i].id,
 		option = createNode('option'),
+		div = createNode('div'),
 		li = createNode('li');
 		option.innerHTML = niewiem;
 			append(li, option);
 			append(ul, option);
+
 				
 button.onclick = function testr(){
-//let dd = ul.options[ul.selectedIndex].value;
-	if(option.id = '2' ){
-	alert('dziala'); // alert(dd); dziala ale to nie o to chodzi
-	}
-else alert('tu wklej przykre slowa');
-}
-}
-						})
+	
+let dd = ul.options[ul.selectedIndex].id; // ID SELECTA KTOREGO MAMY SELECTED
+	let id_api = data[dd-1].id,
+		name_api = data[dd-1].name,
+		symbol_api = data[dd-1].symbol, // symbol api
+		rank_api = data[dd-1].rank,
+		is_new_api = data[dd-1].is_new,
+		is_active_api = data[dd-1].is_active;
+	
+	let	woo = ('ID: ' + id_api + '<br> Name: ' + name_api + '<br> Symbol: ' + symbol_api + '<br> Rank: ' + rank_api + '<br> Is new: ' + is_new_api +  '<br> Is avtive: ' + is_active_api);
+		div.innerHTML = woo;
+			append(divik, div);
+
+
+								} //end of buttons function
+									} //end of loop
+						})//end of fetch
 .catch(function(error) {
   console.log(error);
 }); 
